35ca28a9c21bdd9a8a8764c16a4cb753d4f92600
Yulia Bezginova <ybezginova2021@gmail.com>
2023-05-09 22:30:21 +0200

    exercises

--- exercises/ex_16_median.py
+++ exercises/ex_16_median.py
@@ -4,4 +4,11 @@
 Найти: медиану массива. Оценить сложность алгоритма по затратам cpu, ram.
 Условия: нельзя использовать сторонние библиотеки. Только чистый python.
 """
+def median(arr):
+    arr_sorted = sorted(arr)
+    n = len(arr_sorted)
+    if n % 2 == 1:
+        return arr_sorted[n//2]
+    else:
+        return (arr_sorted[n//2 - 1] + arr_sorted[n//2]) / 2
 
